9eff33bd62d8dcf03023f3adf7c2b74133bfb6be
siddheshtodkar <stodkar53@gmail.com>
2025-09-10 20:51:26 +0530

    tanstack query to fetch idea and axios

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -1,18 +1,33 @@
-import { createFileRoute } from '@tanstack/react-router'
+import api from '@/lib/axios'
+import type { Idea } from '@/types'
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
-const fetchIdea = async ({ params }) => {
-  const res = await fetch(`/api/ideas/${params.ideaId}`)
-  if (!res.ok)
-    throw new Error('Failed to fetch ideas')
-  return res.json()
+const fetchIdea = async (ideaId: string): Promise<Idea> => {
+  const res = await api.get(`/ideas/${ideaId}`)
+  return res.data
 }
+
+const ideaQueryOptions = (ideaId: string) => queryOptions({
+  queryKey: ['idea', ideaId],
+  queryFn: () => fetchIdea(ideaId)
+})
 
 export const Route = createFileRoute('/ideas/$ideaId/')({
   component: IdeaDetailsPage,
-  loader: fetchIdea
+  loader: async ({ params, context: { queryClient } }) => {
+    return queryClient.ensureQueryData(ideaQueryOptions(params.ideaId))
+  }
 })
 
 function IdeaDetailsPage() {
-  const idea = Route.useLoaderData()
-  return <div>Hello {idea.title}!</div>
+  const { ideaId } = Route.useParams()
+  const { data: idea } = useSuspenseQuery(ideaQueryOptions(ideaId))
+  return (
+    <div className="p-4">
+      <Link to='/ideas' className='text-blue-500 underline block mb-4'>Back To Ideas</Link>
+      <h2 className="text-2xl font-bold">{idea.title}</h2>
+      <p className="mt-2">{idea.description}</p>
+    </div>
+  )
 }
