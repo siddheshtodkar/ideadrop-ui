74cd9c289c284ac6a1a38b5491b137db03fa1fd7
siddheshtodkar <stodkar53@gmail.com>
2025-10-05 11:47:07 +0530

    register and login API integration

---
+++ src/api/auth.ts
@@ -0,0 +1,21 @@
+import api from "@/lib/axios"
+
+export const registerUser = async (credentials: { name: string, email: string, password: string }) => {
+  try {
+    const res = await api.post('/auth/register', credentials)
+    return res.data
+  } catch (error: any) {
+    const message = error.response?.data?.message || 'Failed to register'
+    throw new Error(message)
+  }
+}
+
+export const loginUser = async (credentials: { email: string, password: string }) => {
+  try {
+    const res = await api.post('/auth/login', credentials)
+    return res.data
+  } catch (error: any) {
+    const message = error.response?.data?.message || 'Failed to login'
+    throw new Error(message)
+  }
+}
--- src/routes/(auth)/login/index.tsx
+++ src/routes/(auth)/login/index.tsx
@@ -1,4 +1,7 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { loginUser } from '@/api/auth'
+import { useAuth } from '@/context/AuthContext'
+import { useMutation } from '@tanstack/react-query'
+import { createFileRoute, Link, useNavigate } from '@tanstack/react-router'
 import { useState } from 'react'
 
 export const Route = createFileRoute('/(auth)/login/')({
@@ -6,17 +9,47 @@ export const Route = createFileRoute('/(auth)/login/')({
 })
 
 function LoginComponent() {
+  const navigate = useNavigate()
+  const { setAccessToken, setUser } = useAuth()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: loginUser,
+    onSuccess: (data) => {
+      setAccessToken(data.accessToken)
+      setUser(data.user)
+      navigate({ to: '/ideas' })
+    },
+    onError: (err) => {
+      setError(err.message)
+    }
+  })
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setError('')
+    try {
+      await mutateAsync({ email, password })
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
   return (
     <div className="max-w-md mx-auto">
       <h1 className='text-3xl font-bold mb-6'>Login</h1>
-      <form className='space-y-4'>
+      {error && (
+        <div className="bg-red-100 text-red-700 px-4 py-2 rounded mb-4">{error}</div>
+      )}
+      <form onSubmit={handleSubmit} className='space-y-4'>
         <input type="text" name="email" id="email" value={email} placeholder='Email' autoComplete='off' onChange={(e) => setEmail(e.target.value)}
           className='w-full border border-gray rounded-md p-2' />
         <input type="text" name="password" id="password" value={password} placeholder='Password' autoComplete='off' onChange={(e) => setPassword(e.target.value)}
           className='w-full border border-gray rounded-md p-2' />
-        <button className='bg-blue-600 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-md w-full disabled:opacity-50'>Login</button>
+        <button className='bg-blue-600 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-md w-full disabled:opacity-50' disabled={isPending}>
+          {isPending ? 'Logging in...' : 'Login'}
+        </button>
       </form>
       <p className='text-sm text-center mt-4'>
         Don't have an account? <Link to='/login' className='text-blue-600 hover:underline font-medium'>Register</Link>
